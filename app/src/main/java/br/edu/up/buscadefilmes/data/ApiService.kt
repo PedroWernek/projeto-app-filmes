83843995547f021f6c5aa6f3a8249862ea588993
PedroWernek <pedrowcnetto@gmail.com>
2025-10-27 17:54:10 -0300

    feat: commit final

--- app/src/main/java/br/edu/up/buscadefilmes/data/ApiService.kt
+++ app/src/main/java/br/edu/up/buscadefilmes/data/ApiService.kt
@@ -3,7 +3,7 @@ package br.edu.up.buscadefilmes.data
 import br.edu.up.buscadefilmes.domain.model.Filme
 import retrofit2.http.GET
 import retrofit2.Response
-import retrofit2.http.PUT
+
 
 interface ApiService {
     @GET("filmes")
